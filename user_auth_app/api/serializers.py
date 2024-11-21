9952c6d6a4a98c7295fe2b3a341d17cdfc68f1bb
Kevin Mueller <kevin.mueller@tuta.com>
2024-11-21 13:02:32 +0100

    commented code

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -9,32 +9,59 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = ['user', 'bio', 'location']
 
 
+# Klasse zum validieren der REgistrierung
 class RegistrationSerializer(serializers.ModelSerializer):
+    """
+    Serializer für die Benutzerregistrierung.
+
+    Eigenschaften:
+    - Validiert die Eingabe von 'password' und 'repeated_password'.
+    - Erstellt einen neuen Benutzer und speichert das Passwort als sicheren Hash.
+
+    Felder:
+    - username: Benutzername des neuen Kontos.
+    - email: E-Mail-Adresse des neuen Benutzers.
+    - password: Passwort des Benutzers (write_only).
+    - repeated_password: Zweites Passwortfeld zur Bestätigung (write_only).
+
+    Hinweise:
+    - 'password' und 'repeated_password' müssen übereinstimmen.
+    - Die E-Mail-Adresse darf nicht bereits registriert sein.
+    """
+    # Serializer Charfield da repeated password nicht teil von User Klasse ist => write_only damit daten nicht zurückgegeben werden
     repeated_password = serializers.CharField(write_only=True)
 
+    #Erstellung der Meta Daten für den Serializer
     class Meta:
         model = User
         fields =['username','email','password','repeated_password']
+        # in extra_kwargs wird das passwort ebenso auf write only gesetzt damit dieser nicht zurückgegeben wird nach der verarbeitung
         extra_kwargs ={
             'password':{
                 'write_only':True
             }
         }
     
+    # definieren der save methode
     def save(self):
-        print(self.validated_data['password'])
+        # weist den beiden variablen die daten aus dem übergebenen datenparameter aus der view zu
         pw = self.validated_data['password']
         repeated_pw=self.validated_data['repeated_password']
         
+        # vergleicht ob beide passwörter matchen wenn nicht wird ein error geraised
         if pw != repeated_pw:
             raise serializers.ValidationError({'error':'passwords dont match'})
         
+        # überprüft ob in allen User Objekten die email irgendwo vorhanden ist, wenn ja wird ein error geraised
         if User.objects.filter(email=self.validated_data['email']).exists():
             raise serializers.ValidationError({'error':'email already exists'})
         
+        # account wird eine User Object instance zugewiesen mit übergebenen daten
         account = User(email = self.validated_data['email'], username = self.validated_data['username'])
+        #passwort wird als hash gesetzt
         account.set_password(pw)
+        # speichert user in der datenbank
         account.save() 
-        
+        # gibt instanz zurück
         return account
         
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -16,24 +16,32 @@ class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
-
+# View zum erhalten von registrierungsdaten und zurückspiegeln des usernames email und token
 class RegistrationView(APIView):
+    # Rechte definition => erlaube alles
     permission_classes = [AllowAny]
 
+    # definieren der Post reaktion
     def post(self, request):
+        #serializer definition mit übergabe der Daten Parameter
         serializer = RegistrationSerializer(data=request.data)
+        #data variable die zurückgespiegelt wird wird initialisiert als leeres Dictionary
         data = {}
 
-
+        # Aktion wenn der Serializer die daten validiert hat und für valide befindet
         if serializer.is_valid():
+            # account instanz aus serializer wird der variable zugewiesen
             saved_account = serializer.save()
+            # holen des tokens über die Token klasse mit übergebenen account -  gibt 2 werte zurück(den token und das erstellungsdatum)
             token, created = Token.objects.get_or_create(user=saved_account)
+            # zuweisung der jeweiligen daten aus account und token
             data = {
                 'token': token.key,
                 'username': saved_account.username,
                 'email': saved_account.email
             }
         else:
+            #ausgabe des Serializer errors wenn daten nicht valide sind
             data=serializer.errors
-            
+        # Antwort auf view anfrage als response -  in diesem fall das data dictionary       
         return Response(data)
